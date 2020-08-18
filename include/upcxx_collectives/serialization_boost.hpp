e7e1946f0c3baf3b2d8713493de386e88531520e
ct.clmsn <ct.clmsn@gmail.com>
2020-08-18 00:34:04 -0400

    added license information to serialization code

--- include/upcxx_collectives/serialization_boost.hpp
+++ include/upcxx_collectives/serialization_boost.hpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2020 Christopher Taylor
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #pragma once
 #ifndef __UPCXX_COLLECTIVES_SERIALIZER_BOOST__
 #define __UPCXX_COLLECTIVES_SERIALIZER_BOOST__
